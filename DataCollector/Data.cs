3b2da2cf9be65bd2e7bf8ee16b90089db9b71804
ChewyMoon <ChewyMoon@users.noreply.github.com>
2016-11-20 09:30:00 -0500

    Bug fixes!

---
+++ DataCollector/Data.cs
@@ -0,0 +1,220 @@
+﻿namespace DataCollector
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    using Newtonsoft.Json;
+
+    using SharpDX;
+
+    internal class Data
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets the allies.
+        /// </summary>
+        /// <value>
+        ///     The allies.
+        /// </value>
+        public IEnumerable<OtherHeroData> Allies
+            =>
+            ObjectManager.Get<Obj_AI_Hero>()
+                .Where(x => x.Team == this.Team && x.NetworkId != this.NetworkId)
+                .Select(x => OtherHeroData.CreateFromHero(x));
+
+        /// <summary>
+        ///     Gets or sets the allies in range.
+        /// </summary>
+        /// <value>
+        ///     The allies in range.
+        /// </value>
+        public double AlliesInRange { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the can attack.
+        /// </summary>
+        /// <value>
+        ///     The can attack.
+        /// </value>
+        public double CanAttack { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the can move.
+        /// </summary>
+        /// <value>
+        ///     The can move.
+        /// </value>
+        public double CanMove { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the direction x.
+        /// </summary>
+        /// <value>
+        ///     The direction x.
+        /// </value>
+        public double DirectionX { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the direction y.
+        /// </summary>
+        /// <value>
+        ///     The direction y.
+        /// </value>
+        public double DirectionY { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the distance to closest ally.
+        /// </summary>
+        /// <value>
+        ///     The distance to closest ally.
+        /// </value>
+        public double DistanceToClosestAlly { get; set; }
+
+        /// <summary>
+        ///     Gets the enemies.
+        /// </summary>
+        /// <value>
+        ///     The enemies.
+        /// </value>
+        public IEnumerable<OtherHeroData> Enemies
+            =>
+            ObjectManager.Get<Obj_AI_Hero>()
+                .Where(x => x.Team != this.Team && x.NetworkId != this.NetworkId)
+                .Select(x => OtherHeroData.CreateFromHero(x));
+
+        /// <summary>
+        ///     Gets or sets the enemies in range.
+        /// </summary>
+        /// <value>
+        ///     The enemies in range.
+        /// </value>
+        public double EnemiesInRange { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the experience.
+        /// </summary>
+        /// <value>
+        ///     The experience.
+        /// </value>
+        public double Experience { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the health percent.
+        /// </summary>
+        /// <value>
+        ///     The health percent.
+        /// </value>
+        public double HealthPercent { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the level.
+        /// </summary>
+        /// <value>
+        ///     The level.
+        /// </value>
+        public double Level { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the mana percent.
+        /// </summary>
+        /// <value>
+        ///     The mana percent.
+        /// </value>
+        public double ManaPercent { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the network identifier.
+        /// </summary>
+        /// <value>
+        ///     The network identifier.
+        /// </value>
+        [JsonIgnore]
+        public int NetworkId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the position x.
+        /// </summary>
+        /// <value>
+        ///     The position x.
+        /// </value>
+        public double PositionX { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the position y.
+        /// </summary>
+        /// <value>
+        ///     The position y.
+        /// </value>
+        public double PositionY { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the recalling.
+        /// </summary>
+        /// <value>
+        ///     The recalling.
+        /// </value>
+        public double Recalling { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the team.
+        /// </summary>
+        /// <value>
+        ///     The team.
+        /// </value>
+        [JsonIgnore]
+        public GameObjectTeam Team { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the under turret.
+        /// </summary>
+        /// <value>
+        ///     The under turret.
+        /// </value>
+        public double UnderTurret { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Creates a <see cref="Data" /> object from an <see cref="Obj_AI_Base" />.
+        /// </summary>
+        /// <param name="hero">The hero.</param>
+        /// <param name="position">The position.</param>
+        /// <returns></returns>
+        public static Data CreateFromHero(Obj_AI_Hero hero, Vector2 position = new Vector2())
+        {
+            position = position.IsZero ? hero.Position.To2D() : position;
+
+            return new Data()
+                       {
+                           PositionX = position.X, PositionY = position.Y, DirectionX = hero.Direction.X,
+                           DirectionY = hero.Direction.Y, HealthPercent = hero.HealthPercent,
+                           DistanceToClosestAlly =
+                               ObjectManager.Get<Obj_AI_Hero>()
+                                   .Where(x => x.Team == hero.Team && x.NetworkId != hero.NetworkId)
+                                   .Select(x => x.Distance(hero))
+                                   .Min(),
+                           AlliesInRange =
+                               ObjectManager.Get<Obj_AI_Hero>()
+                                   .Where(x => x.Team == hero.Team && x.NetworkId != hero.NetworkId)
+                                   .Count(x => hero.Distance(x) <= 2000),
+                           EnemiesInRange =
+                               ObjectManager.Get<Obj_AI_Hero>()
+                                   .Where(x => x.Team != hero.Team)
+                                   .Count(x => hero.Distance(x) <= 2000),
+                           Level = hero.Level,
+                           Experience = hero.Experience, CanMove = hero.CanMove ? 1 : 0,
+                           CanAttack = hero.CanAttack ? 1 : 0, Recalling = hero.IsRecalling() ? 1 : 0,
+                           UnderTurret = hero.UnderTurret(true) ? 1 : 0, ManaPercent = hero.ManaPercent,
+                           Team = hero.Team, NetworkId = hero.NetworkId
+                       };
+        }
+
+        #endregion
+    }
+}
--- DataCollector/Program.cs
+++ DataCollector/Program.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Threading.Tasks;
 
     using LeagueSharp;
@@ -24,6 +23,15 @@
         private static string JsonFilePath => Path.Combine(Config.AppDataDirectory, "LAIMP", "Data.json");
 
         /// <summary>
+        ///     Gets the json settings.
+        /// </summary>
+        /// <value>
+        ///     The json settings.
+        /// </value>
+        private static JsonSerializerSettings JsonSettings
+            => new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+
+        /// <summary>
         ///     Gets or sets the queue.
         /// </summary>
         /// <value>
@@ -41,6 +49,12 @@
         /// <param name="args">The arguments.</param>
         private static void Main(string[] args)
         {
+            Directory.CreateDirectory(Path.Combine(Config.AppDataDirectory, "LAIMP"));
+            if (!File.Exists(JsonFilePath))
+            {
+                File.Create(JsonFilePath).Dispose();
+            }
+
             CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
 
@@ -50,23 +64,23 @@
         /// <param name="args">The <see cref="EventArgs" /> instance containing the event data.</param>
         private static void OnGameLoad(EventArgs args)
         {
-            Obj_AI_Base.OnIssueOrder += OnIssueOrder;
+            Obj_AI_Base.OnNewPath += OnNewPath;
             Task.Factory.StartNew(ProcessQueue, TaskCreationOptions.LongRunning);
         }
 
         /// <summary>
-        ///     Called when an <see cref="Obj_AI_Base" /> issues an order.
+        /// Called when a hero has a new path.
         /// </summary>
         /// <param name="sender">The sender.</param>
-        /// <param name="args">The <see cref="GameObjectIssueOrderEventArgs" /> instance containing the event data.</param>
-        private static void OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+        /// <param name="args">The <see cref="GameObjectNewPathEventArgs"/> instance containing the event data.</param>
+        private static void OnNewPath(Obj_AI_Base sender, GameObjectNewPathEventArgs args)
         {
-            if (!sender.IsEnemy || sender.Type != GameObjectType.obj_AI_Hero)
+            if (!(sender is Obj_AI_Hero))
             {
                 return;
             }
 
-            Queue.Enqueue(Data.CreateFromHero((Obj_AI_Hero)sender));
+            Queue.Enqueue(Data.CreateFromHero((Obj_AI_Hero)sender, args.Path[args.Path.Length - 1].To2D()));
         }
 
         /// <summary>
@@ -81,215 +95,17 @@
                     var data = Queue.Dequeue();
 
                     // todo cache this list
-                    var dataList = JsonConvert.DeserializeObject<List<Data>>(File.ReadAllText(JsonFilePath));
+                    var dataList = JsonConvert.DeserializeObject<List<Data>>(
+                                       File.ReadAllText(JsonFilePath),
+                                       JsonSettings) ?? new List<Data>();
+
                     dataList.Add(data);
 
-                    File.WriteAllText(JsonFilePath, JsonConvert.SerializeObject(dataList));
+                    File.WriteAllText(JsonFilePath, JsonConvert.SerializeObject(dataList, JsonSettings));
+
+                    Console.WriteLine("Logged!");
                 }
             }
-        }
-
-        #endregion
-    }
-
-    internal class Data
-    {
-        #region Public Properties
-
-        /// <summary>
-        ///     Gets the allies.
-        /// </summary>
-        /// <value>
-        ///     The allies.
-        /// </value>
-        public IEnumerable<Data> Allies
-            =>
-            ObjectManager.Get<Obj_AI_Hero>()
-                .Where(x => x.Team == this.Team && x.NetworkId != this.NetworkId)
-                .Select(CreateFromHero);
-
-        /// <summary>
-        ///     Gets or sets the allies in range.
-        /// </summary>
-        /// <value>
-        ///     The allies in range.
-        /// </value>
-        public double AlliesInRange { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the can attack.
-        /// </summary>
-        /// <value>
-        ///     The can attack.
-        /// </value>
-        public double CanAttack { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the can move.
-        /// </summary>
-        /// <value>
-        ///     The can move.
-        /// </value>
-        public double CanMove { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the direction x.
-        /// </summary>
-        /// <value>
-        ///     The direction x.
-        /// </value>
-        public double DirectionX { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the direction y.
-        /// </summary>
-        /// <value>
-        ///     The direction y.
-        /// </value>
-        public double DirectionY { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the distance to closest ally.
-        /// </summary>
-        /// <value>
-        ///     The distance to closest ally.
-        /// </value>
-        public double DistanceToClosestAlly { get; set; }
-
-        /// <summary>
-        ///     Gets the enemies.
-        /// </summary>
-        /// <value>
-        ///     The enemies.
-        /// </value>
-        public IEnumerable<Data> Enemies
-            => ObjectManager.Get<Obj_AI_Hero>().Where(x => x.Team != this.Team).Select(CreateFromHero);
-
-        /// <summary>
-        ///     Gets or sets the enemies in range.
-        /// </summary>
-        /// <value>
-        ///     The enemies in range.
-        /// </value>
-        public double EnemiesInRange { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the experience.
-        /// </summary>
-        /// <value>
-        ///     The experience.
-        /// </value>
-        public double Experience { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the health percent.
-        /// </summary>
-        /// <value>
-        ///     The health percent.
-        /// </value>
-        public double HealthPercent { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the level.
-        /// </summary>
-        /// <value>
-        ///     The level.
-        /// </value>
-        public double Level { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the mana percent.
-        /// </summary>
-        /// <value>
-        ///     The mana percent.
-        /// </value>
-        public double ManaPercent { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the network identifier.
-        /// </summary>
-        /// <value>
-        ///     The network identifier.
-        /// </value>
-        [JsonIgnore]
-        public int NetworkId { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the position x.
-        /// </summary>
-        /// <value>
-        ///     The position x.
-        /// </value>
-        public double PositionX { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the position y.
-        /// </summary>
-        /// <value>
-        ///     The position y.
-        /// </value>
-        public double PositionY { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the recalling.
-        /// </summary>
-        /// <value>
-        ///     The recalling.
-        /// </value>
-        public double Recalling { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the team.
-        /// </summary>
-        /// <value>
-        ///     The team.
-        /// </value>
-        [JsonIgnore]
-        public GameObjectTeam Team { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the under turret.
-        /// </summary>
-        /// <value>
-        ///     The under turret.
-        /// </value>
-        public double UnderTurret { get; set; }
-
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        ///     Creates a <see cref="Data" /> object from an <see cref="Obj_AI_Base" />.
-        /// </summary>
-        /// <param name="hero">The hero.</param>
-        /// <returns></returns>
-        public static Data CreateFromHero(Obj_AI_Hero hero)
-        {
-            return new Data()
-                       {
-                           PositionX = hero.ServerPosition.X, PositionY = hero.ServerPosition.Y,
-                           DirectionX = hero.Direction.X, DirectionY = hero.Direction.Y,
-                           HealthPercent = hero.HealthPercent,
-                           DistanceToClosestAlly =
-                               ObjectManager.Get<Obj_AI_Hero>()
-                                   .Where(x => x.Team == hero.Team && x.NetworkId != hero.NetworkId)
-                                   .Select(x => x.Distance(hero))
-                                   .Min(),
-                           AlliesInRange =
-                               ObjectManager.Get<Obj_AI_Hero>()
-                                   .Where(x => x.Team == hero.Team && x.NetworkId != hero.NetworkId)
-                                   .Count(x => hero.Distance(x) <= 1000) - 1,
-                           EnemiesInRange =
-                               ObjectManager.Get<Obj_AI_Hero>()
-                                   .Where(x => x.Team != hero.Team)
-                                   .Count(x => hero.Distance(x) <= 1000) - 1,
-                           Level = hero.Level,
-                           Experience = hero.Experience, CanMove = hero.CanMove ? 1 : 0,
-                           CanAttack = hero.CanAttack ? 1 : 0, Recalling = hero.IsRecalling() ? 1 : 0,
-                           UnderTurret = hero.UnderTurret(true) ? 1 : 0, ManaPercent = hero.ManaPercent,
-                           Team = hero.Team, NetworkId = hero.NetworkId
-                       };
         }
 
         #endregion
